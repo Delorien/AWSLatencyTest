a409e3c492e2c5ea792685836f00cebf1bf3c735
Leonardo Tonin <=>
2016-06-14 19:17:01 -0300

    Fix import

--- src/main/java/com/payu/testator/AWSLatencyTest/Application.java
+++ src/main/java/com/payu/testator/AWSLatencyTest/Application.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.NoSuchElementException;
 
-import com.amazonaws.services.kinesisfirehose.model.InvalidArgumentException;
-
 public class Application {
 
 	private final BufferedReader reader;
